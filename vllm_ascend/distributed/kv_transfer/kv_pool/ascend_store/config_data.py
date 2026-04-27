3fa1e280fca516416400c784b704e1417ff2971c
F.Liu <1661888967@qq.com>
2026-04-28 02:02:48 +0800

    clean code
    Signed-off-by: F.Liu <1661888967@qq.com>

--- vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/config_data.py
+++ vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/config_data.py
@@ -525,7 +525,7 @@ class RequestTracker:
     chunk_gvas: list[int] = field(default_factory=list)
     last_block_gva: int | None = None
 
-    block_keys: list[str] | None = None
+    block_keys: list[str] = field(default_factory=list)
 
     starts: list[int] | None = None
     ends: list[int] | None = None
@@ -706,8 +706,9 @@ class ReqMeta:
             is_last_chunk=is_last_chunk,
             token_ids=token_ids,
             original_block_size=original_block_size,
-            kv_cache_group_ids=list(range(len(tracker.allocated_block_ids_by_group))),
-            kv_cache_families_by_group=kv_cache_group_families,
+            # TODO is this necessary?
+            chunk_gvas=tracker.chunk_gvas.copy(),
+            last_block_gva=tracker.last_block_gva,
         )
 
 
