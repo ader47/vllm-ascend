43d88f27868a7c5792c249a4682c9e0e53e047f0
F.Liu <1661888967@qq.com>
2026-05-19 19:49:19 +0800

    del old lookup scheduler
    Signed-off-by: F.Liu <1661888967@qq.com>

--- vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/ascend_store_connector.py
+++ vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/ascend_store_connector.py
@@ -1,9 +1,7 @@
-import threading
 from collections.abc import Iterable
 from typing import Any
 
 import torch
-import zmq
 from vllm.config import VllmConfig
 from vllm.distributed.kv_events import (
     KVCacheEvent,
@@ -18,7 +16,6 @@ from vllm.distributed.kv_transfer.kv_connector.v1.base import (
 )
 from vllm.forward_context import ForwardContext
 from vllm.logger import logger
-from vllm.utils.network_utils import make_zmq_socket
 from vllm.v1.attention.backend import AttentionMetadata  # type: ignore
 from vllm.v1.core.block_pool import BlockPool
 from vllm.v1.core.kv_cache_manager import KVCacheBlocks
@@ -26,12 +23,10 @@ from vllm.v1.core.sched.output import SchedulerOutput
 from vllm.v1.kv_cache_interface import KVCacheConfig
 from vllm.v1.outputs import KVConnectorOutput
 from vllm.v1.request import Request
-from vllm.v1.serial_utils import MsgpackDecoder
 
 from vllm_ascend.distributed.kv_transfer.kv_pool.ascend_store.config_data import AscendStoreKVConnectorWorkerMetadata
 from vllm_ascend.distributed.kv_transfer.kv_pool.ascend_store.pool_scheduler import (
     KVPoolScheduler,
-    get_zmq_rpc_path_lookup,
 )
 from vllm_ascend.distributed.kv_transfer.kv_pool.ascend_store.pool_worker import KVPoolWorker
 
@@ -112,8 +107,6 @@ class AscendStoreConnector(KVConnectorBase_V1, SupportsHMA):
             )
             # self.connector_worker.layer_transfer_finished_events = vllm_config.layer_transfer_finished_events
             assert self.connector_worker is not None
-            if vllm_config.parallel_config.rank == 0:
-                self.lookup_server = LookupKeyServer(self.connector_worker, vllm_config, self.use_layerwise)
 
     ############################################################
     # Scheduler Side Methods
@@ -268,63 +261,3 @@ class AscendStoreConnector(KVConnectorBase_V1, SupportsHMA):
         ascend_store_kv_events = AscendStoreKVEvents(num_workers=1)
         ascend_store_kv_events.add_events(events)
         return ascend_store_kv_events
-
-    def bind_gpu_block_pool(self, gpu_block_pool: "BlockPool") -> None:
-        assert self.connector_scheduler is not None
-        self.connector_scheduler.bind_gpu_block_pool(gpu_block_pool)
-
-    def build_connector_worker_meta(self) -> AscendStoreKVConnectorWorkerMetadata | None:
-        assert self.connector_worker is not None
-        return self.connector_worker.build_connector_worker_meta()
-
-
-class LookupKeyServer:
-    def __init__(
-        self,
-        pool_worker: KVPoolWorker,
-        vllm_config: "VllmConfig",
-        use_layerwise: bool,
-    ):
-        self.decoder = MsgpackDecoder()
-        self.decoder_tensor = MsgpackDecoder(torch.Tensor)
-        self.ctx = zmq.Context()  # type: ignore[attr-defined]
-        socket_path = get_zmq_rpc_path_lookup(vllm_config)
-        self.socket = make_zmq_socket(
-            self.ctx,
-            socket_path,
-            zmq.REP,  # type: ignore[attr-defined]
-            bind=True,
-        )
-
-        self.pool_worker = pool_worker
-        self.running = True
-        self.use_layerwise = use_layerwise
-
-        def process_request():
-            while self.running:
-                all_frames = self.socket.recv_multipart(copy=False)
-                token_len = int.from_bytes(all_frames[0], byteorder="big")
-                kv_group_ids = self.decoder.decode([all_frames[1]])
-                hash_frames = all_frames[2:]
-                hashes_str = self.decoder.decode(hash_frames)
-                result = self.pool_worker.lookup_scheduler(
-                    token_len,
-                    hashes_str,
-                    kv_group_ids,
-                    self.use_layerwise,
-                )
-                logger.debug(
-                    "KV pool lookup response token_len=%d groups=%s hit_tokens=%d",
-                    token_len,
-                    kv_group_ids,
-                    result,
-                )
-                response = result.to_bytes(4, "big")
-                self.socket.send(response)
-
-        self.thread = threading.Thread(target=process_request, daemon=True)
-        self.thread.start()
-
-    def close(self):
-        self.socket.close(linger=0)
-        # TODO: close the thread!
