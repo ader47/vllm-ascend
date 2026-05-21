f609608e003b6e163319b07fdafcfa9455ca12a7
F.Liu <1661888967@qq.com>
2026-05-21 19:20:18 +0800

    bind memcache client thread cpus
    Signed-off-by: F.Liu <1661888967@qq.com>

--- vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/pool_worker.py
+++ vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/pool_worker.py
@@ -177,15 +177,15 @@ class KVPoolWorker:
         backend_module = importlib.import_module(backend_path)
         real_backend = getattr(backend_module, backend_name)
 
-        backend_kwargs = {}
-        if self.backend.lower() in {"mooncake", "memcache"}:
-            # DSV4 exposes compress_ratios; only use lazy store init for this
-            # compressed-model path.
-            backend_kwargs["lazy_init"] = self.use_compress
-        self.m_store = real_backend(  # type: ignore[misc]
-            parallel_config,
-            **backend_kwargs,
-        )
+        if self.backend.lower() == "memcache":
+            self.m_store = real_backend(  # type: ignore[misc]
+                parallel_config,
+                extra_config.get("memcache_client_cpus"),
+            )
+        else:
+            self.m_store = real_backend(  # type: ignore[misc]
+                parallel_config
+            )
         kv_event_config = vllm_config.kv_events_config
         self.enable_kv_events = False
         if kv_event_config and kv_event_config.enable_kv_cache_events:
