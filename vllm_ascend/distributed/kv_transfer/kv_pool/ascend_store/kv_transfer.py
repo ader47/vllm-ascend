4d0db9208bc2783fb6449d01a03df1ba0eef1ee4
Feng Liu <liufeng248@huawei.com>
2026-05-30 22:57:27 +0800

    fixbug
    Signed-off-by: Feng Liu <liufeng248@huawei.com>

--- vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/kv_transfer.py
+++ vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/kv_transfer.py
@@ -1119,20 +1119,21 @@ class KVCacheStoreLayerRecvingThread(KVTransferThread):
             return False
         assert self.d2d_broadcast_group is not None
         group = self.d2d_broadcast_group
+        device_group = group.device_group
         is_reader = self.h2d_reader_group is not None and self.h2d_reader_group.rank_in_group == 0
         with torch.npu.stream(self._cooperative_load_stream):
             for kv_cache, staging_buffers, slot_mapping, block_start, block_end in chunks:
                 if is_reader:
                     reqs = []
-                    for dst in range(1, group.size()):
+                    for dst in range(1, group.world_size):
                         for staging in staging_buffers:
-                            reqs.append(dist.isend(staging, dst=dst, group=group))
+                            reqs.append(dist.isend(staging, dst=dst, group=device_group))
                     for req in reqs:
                         req.wait()
                 else:
                     reqs = []
                     for staging in staging_buffers:
-                        reqs.append(dist.irecv(staging, src=0, group=group))
+                        reqs.append(dist.irecv(staging, src=0, group=device_group))
                     for req in reqs:
                         req.wait()
                 self._write_typed_parts_to_kv_cache(
